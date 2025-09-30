edee1610bf7ee92dab88caef9325e5ff722185b0
lmohin <loic.mohin@gmail.com>
2025-09-30 03:44:43 +0200

    fixed big issues with lps query, added true lp calculator

--- snapshot.py
+++ snapshot.py
@@ -10,58 +10,110 @@ class User:
         self.username = username
         self.tag = tag
         self.puuid = puuid
+        self.tier = None
+        self.rank = 0
+        self.lps = 0
+        self.adjustedLps = 0
 
-async def getPuuid(username, tag, api_key):
-    """'user' (string) must designs a valid riot ID"""
-    api_uri = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + username + "/" + tag
+    def calculateAdjustedLps(self):
+        match self.tier:
+            case "MASTER" | "GRANDMASTER" | "CHALLENGER":
+                self.adjustedLps = self.lps
+            case "DIAMOND":
+                self.adjustedLps = 100 -(100 - self.lps) - 100 * self.rank
+            case "EMERALD":
+                self.adjustedLps = -300 - (100 - self.lps) - 100 * self.rank
+            case "PLATINUM":
+                self.adjustedLps = -700 - (100 - self.lps) - 100 * self.rank
+            case "GOLD":
+                self.adjustedLps = -1100 - (100 - self.lps) - 100 * self.rank
+            case "SILVER":
+                self.adjustedLps = -1500 - (100 - self.lps) - 100 * self.rank
+            case "BRONZE":
+                self.adjustedLps = -1900 - (100 - self.lps) - 100 * self.rank
+            case "IRON":
+                self.adjustedLps = -2300 - (100 - self.lps) - 100 * self.rank
+            case _:
+                self.adjustedLps = -2801
+
+
+async def getPuuid(user, api_key):
+    """'user is an instance of User"""
+    api_uri = f"https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{user.username}/{user.tag}"
     headers = {
             "X-Riot-Token": api_key
     }
     async with aiohttp.ClientSession() as session:
         async with session.get(api_uri, headers=headers) as response:
             if response.status != 200:
-                print(f"Error: {response.status} ({username}#{tag})")
-                if response.status == 404:
-                    return
-                await asyncio.sleep(1)
-                return await printUserInfos(user, tag)
+                print(f"Error: {response.status} ({user.username}#{user.tag})")
+                return None
             datas = await response.json()
-            puuid = datas["puuid"]
-    return puuid
+            user.puuid = datas["puuid"]
 
-async def printUserInfos(user, api_key):
-    async with aiohttp.ClientSession() as session:
-        headers = {
-                "X-Riot-Token": api_key
-        }
-        api_uri = "https://euw1.api.riotgames.com/tft/league/v1/by-puuid/"
-        api_uri += user.puuid
-        async with session.get(api_uri, headers=headers) as response:
-            if response.status != 200:
-                print(f"Error: {response.status} ({user.username}#{user.tag})")
-                return
-            datas = (await response.json())
-            if len(datas) == 0:
-                return
-            datas = datas[0]
-            print(user.username, user.tag, datas['tier'], datas['rank'], datas['leaguePoints'])
-        api_uri  = f"https://europe.api.riotgames.com/tft/match/v1/matches/by-puuid/{user.puuid}/ids"
-        async with session.get(api_uri, headers=headers) as response:
-            if response.status != 200:
-                print(f"Error: {response.status} ({user.username}#{user.tag})")
-                return
-            datas = (await response.json())
-            matchId = datas[0]
-        api_uri = f"https://europe.api.riotgames.com/tft/match/v1/matches/{matchId}"
-        async with session.get(api_uri, headers=headers) as response:
-            if response.status != 200:
-                print(f"Error match: {response.status} ({user.username}#{user.tag})")
-                return
-            datas = await response.json()
-            game = datas["info"]["participants"]
-            for i in range(8):
-                playerInfo = game[i]
-                print(playerInfo["riotIdGameName"], playerInfo["riotIdTagline"], playerInfo["placement"], 9 - playerInfo["placement"])
+async def getRank(user, session, api_key):
+    if user.puuid == None:
+        return
+    headers = {
+        "X-Riot-Token": api_key
+    }
+    api_uri = f"https://euw1.api.riotgames.com/tft/league/v1/by-puuid/{user.puuid}"
+    async with session.get(api_uri, headers=headers) as response:
+        if response.status != 200:
+            print(f"Error: {response.status} ({user.username}#{user.tag})")
+            return
+        datas = (await response.json())
+        if len(datas) == 0:
+            print(user.username, "no lp")
+            return
+        rankedDatas = None
+        for tempDatas in datas:
+            if tempDatas['queueType'] == 'RANKED_TFT':
+                rankedDatas = tempDatas
+                break
+        datas = rankedDatas
+        if datas == None:
+            print(user.username, 'no lp')
+            return
+        user.tier = datas['tier']
+        match datas['rank']:
+            case "I":
+                rank = 1
+            case "II":
+                rank = 2
+            case "III":
+                rank = 3
+            case "IV":
+                rank = 4
+        user.rank = rank
+        user.lps = datas['leaguePoints']
+        user.calculateAdjustedLps()       
+        print(user.username + "#" + user.tag, datas['tier'], datas['rank'], datas['leaguePoints'], user.adjustedLps)
+
+async def printUserInfos(user, session, api_key):
+    headers = {
+        "X-Riot-Token": api_key
+    }
+    if user.puuid == None:
+        return
+    await getRank(user, session, headers)
+    api_uri  = f"https://europe.api.riotgames.com/tft/match/v1/matches/by-puuid/{user.puuid}/ids"
+    async with session.get(api_uri, headers=headers) as response:
+        if response.status != 200:
+            print(f"Error: {response.status} ({user.username}#{user.tag})")
+            return
+        datas = (await response.json())
+        matchId = datas[0]
+    api_uri = f"https://europe.api.riotgames.com/tft/match/v1/matches/{matchId}"
+    async with session.get(api_uri, headers=headers) as response:
+        if response.status != 200:
+            print(f"Error match: {response.status} ({user.username}#{user.tag})")
+            return
+        datas = await response.json()
+        game = datas["info"]["participants"]
+        for i in range(len(game)):
+            playerInfo = game[i]
+            print(playerInfo["riotIdGameName"], playerInfo["riotIdTagline"], playerInfo["placement"], 9 - playerInfo["placement"])
 
 async def main():
     api_key = os.environ.get('RIOT_API_KEY')
@@ -69,19 +121,34 @@ async def main():
         print("Error: no riot api key detected")
         return
     task = []
-    puuid = await getPuuid("Falkor", "Genti", api_key)
-    falkor = User("Falkor", "Genti", puuid)
-    for i in range(1):
-        #task.append(printUserInfos("Toonutv", "EUW", api_key))
-        task.append(printUserInfos(falkor, api_key))
-        #task.append(printUserInfos("Megata", "0000", api_key))
-        #task.append(printUserInfos("Essrog", "TFT", api_key))
-        #task.append(printUserInfos("Arestidios", "AAA", api_key))
-        #task.append(printUserInfos("Azzo", "009", api_key))
-        #task.append(printUserInfos("Yottah", "0000", api_key))
-        #task.append(printUserInfos("brozilla", "brz", api_key))
-        #task.append(printUserInfos("etre infame", "EUW", api_key))
-    results = await asyncio.gather(*task)
+    users = []
+    users.append(User("Falkor", "Genti"))
+    users.append(User("Megata", "0000"))
+    users.append(User("Yottah", "0000"))
+    users.append(User("Arestidios", "AAA"))
+    users.append(User("Patrick", "888"))
+    users.append(User("Lornyk", "888"))
+    users.append(User("Higa", "gmgn"))
+    users.append(User("xRynn", "EUW"))
+    users.append(User("giselle", "purr"))
+    users.append(User("grizzly", "4896"))
+    users.append(User("Cysteine", "EUW"))
+    users.append(User("La Viky", "EUW"))
+    users.append(User("Nikulol", "EUW"))
+    users.append(User("true86", "EUW"))
+    users.append(User("cZxHSusie", "EUW"))
+    users.append(User("RCS Xperion", "EUW11"))
+    users.append(User("QAQ Hecaa", "EUW"))
+    users.append(User("Bambilex", "EUW"))
+    for user in users:
+        if user.puuid == None:
+            task.append(getPuuid(user, api_key))
+    await asyncio.gather(*task)
+    async with aiohttp.ClientSession() as session:
+        task = []
+        for user in users:
+            task.append(getRank(user, session, api_key))
+        await asyncio.gather(*task)
 
 if __name__ == "__main__":
       asyncio.run(main())
